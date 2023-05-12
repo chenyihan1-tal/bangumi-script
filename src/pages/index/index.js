d01c1dbec69c68dfb3871ca366b46d99de70a635
Xavier <xavierror@aliyun.com>
2023-05-12 11:14:01 +0800

    ft: 屏蔽吐槽箱

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,76 +1,85 @@
 const IndexPage = () => {
-  console.log('首页');
+  console.log("首页");
 
-  document.querySelector("#home_tml")?.remove()
-  document.querySelector("#columnHomeB > div.sideInner")?.remove()
-  document.querySelector("#columnHomeB > div:nth-child(2)")?.remove()
-  document.querySelector("#listWrapper > div > div.jspVerticalBar")?.remove()
+  document.querySelector("#home_tml")?.remove();
+  document.querySelector("#columnHomeB")?.remove?.();
+  document.querySelector("#prgManagerMode")?.remove?.();
+  document.querySelector("#listWrapper > div > div.jspVerticalBar")?.remove();
 
-  document.querySelector("#prgManagerMain").style.height = '80vh'
-  document.querySelector("#prgManagerMain").style.display = 'flex '
-  document.querySelector("#listWrapper > div").style.height = '80vh'
-  document.querySelector("#listWrapper").style.height = '80vh'
+  document.querySelector("#prgManagerMain").style.height = "80vh";
+  document.querySelector("#prgManagerMain").style.display = "flex ";
+  document.querySelector("#listWrapper > div").style.height = "80vh";
+  document.querySelector("#listWrapper").style.height = "80vh";
 
-  const prgSubjectList = document.querySelector("#prgSubjectList")
+  const prgSubjectList = document.querySelector("#prgSubjectList");
   if (prgSubjectList) {
-    prgSubjectList.style.height = '80vh'
-    prgSubjectList.style.width = '350px'
-    prgSubjectList.style.overflow = 'scroll'
-    prgSubjectList.style.display = 'flex'
-    prgSubjectList.style.flexDirection = 'column'
+    prgSubjectList.style.height = "80vh";
+    prgSubjectList.style.width = "350px";
+    prgSubjectList.style.overflow = "scroll";
+    prgSubjectList.style.display = "flex";
+    prgSubjectList.style.flexDirection = "column";
   }
 
-  const prgManagerMain = document.querySelector("#prgManagerMain > div.cloumnSubjects.column")
+  const prgManagerMain = document.querySelector(
+    "#prgManagerMain > div.cloumnSubjects.column"
+  );
   if (prgManagerMain) {
-    prgManagerMain.style.width = 'fit-content'
-    prgManagerMain.replaceChildren(document.querySelector("#prgSubjectList"))
+    prgManagerMain.style.width = "fit-content";
+    prgManagerMain.replaceChildren(document.querySelector("#prgSubjectList"));
   }
 
-  document.querySelector("#main").style.width = '90vw'
-  document.querySelector("#main > div.columns.clearit").style.width = '90vw'
-  document.querySelector("#columnHomeA").style.width = '90vw'
+  document.querySelector("#main").style.width = "90vw";
+  document.querySelector("#main > div.columns.clearit").style.width = "90vw";
+  document.querySelector("#columnHomeA").style.width = "90vw";
 
-  // SidePanel
-  const columnHomeB = document.querySelector("#columnHomeB")
-  if (columnHomeB) {
-    columnHomeB.style.position = 'fixed'
-    columnHomeB.style.right = '0'
-    columnHomeB.style.top = '0'
-    columnHomeB.style.opacity = '0.5'
-  }
+  document.querySelector("#cloumnSubjectInfo").style.width = "76%";
+  document
+    .querySelectorAll("#cloumnSubjectInfo .blockMode")
+    .forEach((i) => (i.style.height = "80vh"));
 
-  document.querySelector("#cloumnSubjectInfo").style.width = '76%'
-  document.querySelectorAll('#cloumnSubjectInfo .blockMode').forEach(i => i.style.height = '80vh')
-
-  document.querySelectorAll("#prgSubjectList li").forEach(i => {
-    const name_e = i.querySelector('a[data-subject-name-cn]')
+  document.querySelectorAll("#prgSubjectList li").forEach((i) => {
+    const name_e = i.querySelector("a[data-subject-name-cn]");
     if (name_e) {
-      const cn_name = name_e.getAttribute('data-subject-name-cn')
-      cn_name && (name_e.querySelector('span').innerText = cn_name)
+      const cn_name = name_e.getAttribute("data-subject-name-cn");
+      cn_name && (name_e.querySelector("span").innerText = cn_name);
     }
 
-    const percent = i.querySelector('.listProgress span')
-    percent.style.background = '#e4959a'
+    const percent = i.querySelector(".listProgress span");
+    percent.style.background = "#e4959a";
 
-    i.style.order = String(1 - Math.floor(Number(percent.style.width.replace('%', ''))))
-  })
+    // i.style.order = String(
+    //   1 - Math.floor(Number(percent.style.width.replace("%", "")))
+    // );
+  });
 
-  document.querySelectorAll("#cloumnSubjectInfo > div .overlay").forEach(i => i.remove())
-  document.querySelectorAll("#cloumnSubjectInfo > div .image").forEach(i => i.style.height = '120px')
-  document.querySelectorAll("#cloumnSubjectInfo > div .pictureFrameGroup").forEach(i => i.style.height = '120px')
-  document.querySelectorAll("#cloumnSubjectInfo > div img").forEach(i => i.setAttribute('src', i.getAttribute('src').replace(/\/c\//, '/l/')))
+  document
+    .querySelectorAll("#cloumnSubjectInfo > div .overlay")
+    .forEach((i) => i.remove());
+  document
+    .querySelectorAll("#cloumnSubjectInfo > div .image")
+    .forEach((i) => (i.style.height = "120px"));
+  document
+    .querySelectorAll("#cloumnSubjectInfo > div .pictureFrameGroup")
+    .forEach((i) => (i.style.height = "120px"));
+  document
+    .querySelectorAll("#cloumnSubjectInfo > div img")
+    .forEach((i) =>
+      i.setAttribute("src", i.getAttribute("src").replace(/\/c\//, "/l/"))
+    );
 
-  document.querySelectorAll(".pictureFrameGroup").forEach(i => {
-    const img = i.querySelector('img')
+  document.querySelectorAll(".pictureFrameGroup").forEach((i) => {
+    const img = i.querySelector("img");
     if (img) {
-      img.style.width = '100px'
-      img.style.height = 'auto'
-      img.style.float = 'left'
-      i.replaceWith(img)
+      img.style.width = "100px";
+      img.style.height = "auto";
+      img.style.float = "left";
+      i.replaceWith(img);
     }
-  })
+  });
 
-  document.querySelectorAll(".headerInner").forEach(i => i.style.marginLeft = '130px')
-}
+  document
+    .querySelectorAll(".headerInner")
+    .forEach((i) => (i.style.marginLeft = "130px"));
+};
 
-export default IndexPage
+export default IndexPage;
--- src/pages/subject/index.js
+++ src/pages/subject/index.js
@@ -1,18 +1,31 @@
 const SubjectPage = () => {
-  console.log('详情页');
+  console.log("详情页");
 
-  const cover = document.querySelector("#bangumiInfo > div > div:nth-child(1) > a > img")
+  const cover = document.querySelector(
+    "#bangumiInfo > div > div:nth-child(1) > a > img"
+  );
   if (cover) {
-    cover.style.width = '100%'
-    cover.setAttribute('src', cover.getAttribute('src').replace(/\/c\//, '/l/'))
+    cover.style.width = "100%";
+    cover.setAttribute(
+      "src",
+      cover.getAttribute("src").replace(/\/c\//, "/l/")
+    );
   }
 
-  const title = document.querySelector("#headerSubject > h1 > a")
+  const title = document.querySelector("#headerSubject > h1 > a");
   if (title) {
     if (title.innerHTML !== title.title) {
-      title.innerHTML = `${title.innerHTML} ${title.title}`
+      title.innerHTML = `${title.title}<br />${title.innerHTML}`;
     }
   }
-}
 
-export default SubjectPage
+  // 吐槽箱
+  if (!location.pathname.endsWith("/comments")) {
+    document.querySelector("#comment_box")?.parentElement?.remove?.();
+  }
+
+  // 分享区
+  document.querySelector(".shareBtn")?.remove?.();
+};
+
+export default SubjectPage;
